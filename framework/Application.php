11e773c1dd1ca1eaa4c412eb8e1569073fca4408
IgorShumeikoMindk <codelifecompany@gmail.com>
2016-02-02 16:21:53 +0200

    test

---
+++ framework/Application.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Framework;
+
+class Application{
+
+
+} 
+
+
+
+
+?>
---
+++ framework/Loader.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Clomment
+ */
+class Loader{
+	protected static $instance = null;
+	protected static $namespaces = array();
+	public static function getInstance(){
+		if(empty(self::$instance)){
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+	public static function load($classname){
+		// @TODO: Add here some registered $namespaces processing...
+		$path = str_replace('Framework','',$classname);
+		$path = __DIR__ . str_replace("\\","/", $path) . '.php';
+		if(file_exists($path)){
+			include_once($path);
+		}
+	}
+	private function __construct(){
+		// Init
+		spl_autoload_register(array(__CLASS__, 'load'));
+	}
+	private function __clone(){
+		// lock
+	}
+	public static function addNamespacePath($namespace, $path){
+		//@TODO: Add here your code
+	}
+}
+Loader::getInstance();
+
+
+?>
